772369cb88f520bd024a17d32c31d10570a301c7
y6310 <hey1hey2hey3go@gmail.com>
2024-02-11 00:00:15 +0900

    first commit

---
+++ src/Times.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Times = () => {
+    const [time setTime] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
+  return (
+    <div>Times</div>
+  )
+}
+
+export default Times
